f64f95bff0709e0f8b42d0ea40b04262fb667905
Sanzid Khan <138652753+SanzidKhan77@users.noreply.github.com>
2023-08-28 16:28:01 +0600

    Innovate-7

---
+++ Innovate-7/js/script.js
@@ -0,0 +1,55 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily:{
+            dm:    ["DM Sans"],
+            chivo: ["Chivo"],
+        },
+        fontSize:{
+            22: "22px",
+            44: "44px",
+            46: "46px",
+            56: "56px",
+        },
+        borderRadius: {
+            20: "20px",
+        },
+        colors: {
+          saasp1:    "#FF7364",
+          saasp2:    "#3639A4",
+          saasp4:    "#2C2643",
+          saasp5:    "#645E76",
+          saasp7:    "#43E7DF",
+          highlight: "#FDB400",
+        },
+        spacing: {
+            26 : "26px ",
+            30 : "30px ",
+            50 : "50px ",
+            100: "100px",
+            116: "116px",
+            140: "140px",
+            163: "163px",
+            195: "195px",
+            200: "200px",
+            291: "291px",
+        },
+        maxWidth: {
+            container: "1410px",
+            796: "796px",
+            576: "576px",
+            553: "553px",
+            530: "530px",
+            504: "504px",
+            467: "467px",
+            300: "300px",
+            220: "220px",
+        },
+        minWidth: {
+            300: "300px",
+            480: "480px",
+            553: "553px",
+        },
+      },
+    },
+  };
